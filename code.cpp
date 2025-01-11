1ee26aef473fb8037463bc1b32b1dd251df04732
badshab <badshab454@gmail.com>
2025-01-11 16:07:07 +0600

    data types

--- code.cpp
+++ code.cpp
@@ -1,12 +1,15 @@
-// c plus plus input
+// c plus plus data types
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int age;
-    cout << "Enter your age: ";
-    cin >> age;
-    cout << "Your age is : " << age << endl;
-    return 0;
+    int myNum = 5; // Integer (whole number)
+    float myFloatNum = 3.14; // Floating point number
+    double myDoubleNum = 10.99; // Double percision floating number
+    char myLetter = 'D'; // Character
+    bool myBoolean = false; // Boolean
+    string myText = "Hello, World!"; // String
+
+    cout << myBoolean << endl;
 }
