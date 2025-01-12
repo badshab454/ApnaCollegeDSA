b0932cdfc50f0bf0cdfab225e41860c56ce91751
badshab <badshab454@gmail.com>
2025-01-12 11:15:20 +0600

    cpp operator

--- code.cpp
+++ code.cpp
@@ -1,15 +1,14 @@
-// c plus plus data types
+// cpp operator
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int myNum = 5; // Integer (whole number)
-    float myFloatNum = 3.14; // Floating point number
-    double myDoubleNum = 10.99; // Double percision floating number
-    char myLetter = 'D'; // Character
-    bool myBoolean = false; // Boolean
-    string myText = "Hello, World!"; // String
-
-    cout << myBoolean << endl;
+    int a = 12;
+    int b = 10;
+    cout << "Sum is: " << a + b << endl;
+    cout << "Substract is: " << a - b << endl;
+    cout << "Product is: " << a * b << endl;
+    cout << "Division is: " << a / b << endl;
+    return 0;
 }
