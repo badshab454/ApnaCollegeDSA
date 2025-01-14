810794cc6377e07529cd9c4d7dd8351db52bd228
badshab <badshab454@gmail.com>
2025-01-14 12:42:49 +0600

    cpp switch case

--- code.cpp
+++ code.cpp
@@ -4,29 +4,15 @@
 using namespace std;
 
 int main() {
-    int day = 1;
-
-    switch (day) {
-        case 1:
-          cout << "Monday";
-          break;
-        case 2:
-          cout << "Tuesday";
-          break;
-        case 3:
-          cout << "Wednesday";
-          break;
-        case 4:
-          cout << "Thursday";
-          break;
-        case 5:
-          cout << "Friday";
-          break;
-        case 6:
-          cout << "Saturday";
-          break;
-        case 7:
-          cout << "Sunday";
-          break;
-    }
+  int num = 7;
+  switch (num) {
+    case 6: 
+      cout << "weekend";
+      break;
+    case 7:
+      cout << "weekend";
+      break;
+    default:
+      cout << "weekday";
+  }
 }
