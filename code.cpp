27084d374961b3742a7f4a2003107a1518fda840
badshab <badshab454@gmail.com>
2025-01-16 11:29:54 +0600

    even numbers

--- code.cpp
+++ code.cpp
@@ -1,9 +1,9 @@
-// cpp for loop
+// cpp even numbers
 #include <iostream>
 using namespace std;
 
 int main() {
-  for (int i = 0; i <= 100; i += 10) {
+  for (int i = 0; i <= 10; i += 2) {
     cout << i << " ";
   }
 }
