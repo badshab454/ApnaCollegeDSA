7aa9cb54b67093601c91d99bfad528958a1a81de
badshab <badshab454@gmail.com>
2025-02-08 12:31:52 +0600

    cpp loop

--- code.cpp
+++ code.cpp
@@ -1,13 +1,14 @@
-// cpp while loop
+// cpp for loop
 
 #include <iostream>
 using namespace std;
 
 int main() {
-  int n = 1;
-  while (n <= 5) {
-    cout << n << endl;
-    n++;
+  int n = 3;
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    sum += i;
   }
+  cout << "sum = " << sum << endl;
   return 0;
 }
