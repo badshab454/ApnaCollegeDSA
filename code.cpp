7257a0a94bb4b7481d68678a9248f4b631b76903
badshab454 <badshab454@gmail.com>
2025-09-27 16:57:24 +0600

    hello dude

--- code.cpp
+++ code.cpp
@@ -1,23 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int decToBinary(int decNum) {
-    int ans = 0, pow = 1;
-    
-    while(decNum > 0) {
-        int rem = decNum % 2;
-        decNum /= 2;
-
-        ans += (rem * pow);
-        pow *= 10;
-    }
-    return ans; // binary form 
-}
-
 int main() {
-    int decNum = 2;
-    for (int i = 1; i <= 10; i++) {
-        cout << decToBinary(i) << endl;
-    }
+    cout << "Hello World!" << endl;
     return 0;
 }
