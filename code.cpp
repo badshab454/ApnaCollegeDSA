b8ef4af6375569638e8c77c002b8940d37d14c24
badshab454 <badshab454@gmail.com>
2025-10-04 17:35:34 +0600

    for loop

--- code.cpp
+++ code.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 int main() {
-    int age = 8;
-    if (age >= 18) {
-        cout << "Can vote" << endl;
-    } else {
-        cout << "Can not vote" << endl;
+    for (int num = 1; num <= 5; num++) {
+        cout << num << " ";      
     }
+    cout << endl;
     return 0;
 }
