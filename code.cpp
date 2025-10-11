86e10e8fcaf5493707edd6e7683cef848d90d514
badshab454 <badshab454@gmail.com>
2025-10-11 18:29:23 +0600

    atcoder 1st solution

--- code.cpp
+++ code.cpp
@@ -1,20 +1,12 @@
-//is prime number?
+//Atcoder contest ABC=>AC
 #include <iostream>
 using namespace std;
 
 int main() {
-    int num = 13;
-    bool isPrime = true;
-    for (int i = 2; i <= num - 1; i++) {
-        if (num % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-    if (isPrime == true) {
-        cout << "Your number is prime: " << num << endl;
-    } else {
-        cout << "Your number is not prime: " << num << endl;
-    }
+    string S;
+    cin >> S;
+    int middle = S.length()/2;
+    S.erase(middle, 1);
+    cout << S << endl;
     return 0;
 }
