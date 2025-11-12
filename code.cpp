cfa87eaf33cb8fdcf9b6a99e46cb1fd3f518e65e
badshab454 <badshab454@gmail.com>
2025-11-12 16:02:07 +0600

    reverse array

--- code.cpp
+++ code.cpp
@@ -1,11 +1,17 @@
-// array
+// Array
 #include <iostream>
 using namespace std;
 
 int main() {
-    int marks[] = {1, 2, 3};
-    int len = sizeof(marks) / sizeof(int);
-    cout << len << endl;
-    
+    int arr[] = {1, 9, 3, 4, 6};
+    int n = sizeof(arr) / sizeof(int);
+    int copyArr[n];
+    for (int i = 0; i < n; i++) {
+        int j = n - i - 1;
+        copyArr[i] = arr[j];
+    }
+    for (int i = 0; i < n; i++) {
+        cout << copyArr[i] << " ";
+    }
     return 0;
 }
